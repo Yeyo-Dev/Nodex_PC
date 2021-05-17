49f76fff18d73be7129b4311c088ec7210902aee
Yeyo <luisabrahamvargas261003@gmail.com>
2021-05-17 11:18:33 -0500

    Arreglos del Xavi

--- js/checkLevel.js
+++ js/checkLevel.js
@@ -5,10 +5,11 @@ fetch("./php/checkLevel.php")
 .then((res)=>{
     console.log(res);
     if (res == 'admin'){
-        document.getElementById('menu').innerHTML = `            <li><a href="almacen.html">Almacén</a></li>
+        document.getElementById('menu').innerHTML = `           
+     <li><a href="almacen.html">Almacén</a></li>
     <li><a href="servicios.html">Servicios</a></li>
     <li><a href="clientes.html">Clientes</a></li>
-    <li><a href="empleados.html">Empleados</a></li> <li>
+    <li><a href="empleados.html">Empleados</a></li> 
     <li><a href="pedidos.html">Pedidos</a></li>
     <li><a href="proveedores.html">Proveedores</a></li>
     <li><a href="usuarios.html">Usuarios</a></li>
